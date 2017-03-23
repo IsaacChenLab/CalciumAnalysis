51f7dee10a2a32c9e67b94b29e19af9bec3e6d8a
Matt Schaff <mschaff@societyforscience.org>
2017-03-23 17:29:35 -0400

    different progressbar paths for diffferent systems

--- FluoroSNNAP_code/AnalyzeData_2.m
+++ FluoroSNNAP_code/AnalyzeData_2.m
@@ -16,8 +16,10 @@ function Data = AnalyzeData_2(selected_folder, real_FPS)
 filenumber = 1;
 if(ispc)
     slash = '\';
+    progresspath = strcat(fileparts(pwd()), slash, 'textprogressbar');
 else
     slash = '/';
+    progresspath = strcat(pwd(), slash, 'textprogressbar');
 end
 if(filenumber>0)
     SOURCE_PATH = [];
@@ -34,9 +36,7 @@ end
 Folder = FNames{1};
 
 % progressbar
-progresspath = strcat(fileparts(pwd()), slash, 'textprogressbar');
 addpath(progresspath);
-
 
 %% For each file in each folder, compute either whole image or individual
 %% cell intensity depending on flag set by ANALYSIS_TYPE
