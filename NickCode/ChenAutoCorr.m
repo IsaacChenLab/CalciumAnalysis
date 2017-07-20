3b19227a1ca65912d6998993865cc0da550ecab3
NickGoel <nckgoel@gmail.com>
2017-07-20 19:30:48 -0400

    Update ChenAutoCorr.m

--- NickCode/ChenAutoCorr.m
+++ NickCode/ChenAutoCorr.m
@@ -1,38 +1,56 @@
-function ChenAutoCorr(cellMatrix, maxLag, binSize, cellsToPlot)
+function ChenAutoCorr(maxLag, binSize, cellsToPlot)
 
-% INPUT 
-%   cellMatrix
-%        C x N matrix where c is the number of cells and N is the number of complete
-%        bins in the time period ananlyzed. output(i,j) is the average spikes/second of
-%        cell i throughout bin j
+% FUNCTION ARGUMENTS 
 %   maxLag = max time(s) to offset the autocorr
-%   binSize = length of time for each bin in cellMatrix
+%   binSize = length of time for each bin in file to be analyzed
 %   cellsToPlot = a vector containing the cell number of each cell to be
 %        plotted
 
-% OUTPUT for each cell
-%   1. plot of firiring rate over time
-%   2. the auto-correllogram
+% IN-FUNCTION PROMPTS
+%   1. .mat file which is output from FC_vs_time() containg a single variable
+%       'binMatrix' which is a 'C x binSize' matrix where C is the number
+%       of cells in the associated recording
+%   2. folder where all the figures generated will be saved
 
-numBins = size(cellMatrix,2);
+% OUTPUT for each cell
+%   1. plot of bin'd firing rate over time
+%   2. the auto-correlelogram
+%   --- figures are displayed and saved in the output folder ---
+
+
+fprintf('Select file to be analyzed...\n');
+[data_file, data_path] = uigetfile('*.mat', 'Select .mat file');
+fprintf('Selected!\n');
+
+fprintf('\nSelect folder where output files should be placed...\n');
+target_folder = uigetdir('', 'Select output folder');
+fprintf('Selected!\n');
+
+load(strcat(data_path, data_file));
+
+numBins = size(binMatrix,2);
 numLags = maxLag/binSize;
 
 for i = cellsToPlot
     
-    figure('Name', strcat("Activity of cell ",num2str(i)), 'NumberTitle', 'off');
+    fName = strcat('Activity_Cell_',num2str(i));
+    f = figure('Name', fName, 'NumberTitle', 'off');
     ax1 = axes;
-    plot(ax1, ((1:numBins)*binSize), cellMatrix(i,:))
+    plot(ax1, ((1:numBins)*binSize), binMatrix(i,:))
     xlabel(ax1, 'Time (s)');
     ylabel(ax1, 'Firing Rate (spikes/s)');
     
-    figure('Name', strcat("AutoCorr of cell ",num2str(i)), 'NumberTitle', 'off');
+    aName = strcat('AutoCorr_Cell_',num2str(i));
+    a = figure('Name', aName, 'NumberTitle', 'off');
     ax2 = axes;
-    r = xcorr(cellMatrix(i,:), numLags, 'coeff');
+    r = xcorr(binMatrix(i,:), numLags, 'coeff');
     auto_x = -maxLag:binSize:maxLag;
     plot(ax2, auto_x, r);
     xlabel(ax2, 'Time offset (s)');
     ylabel(ax2, 'Correlation Coeff');
     
+    saveas(f, strcat(target_folder, '/', fName, '.jpg'));
+    saveas(a, strcat(target_folder, '/', aName, '.jpg'));
 end
 
 end
